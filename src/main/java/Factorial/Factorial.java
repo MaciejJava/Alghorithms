dc00ca76d0ecc0dce057df0114ddeaa043b6fd39
Maciej <mac.laurek@gmail.com>
2017-08-06 11:30:03 +0200

    Factorial done

---
+++ src/main/java/Factorial/Factorial.java
@@ -0,0 +1,24 @@
+package Factorial;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+
+        Scanner myScanner = new Scanner(System.in);
+
+        int n;
+        int silnia = 1;
+
+        System.out.println("Wprowadź liczbę: ");
+        n = myScanner.nextInt();
+        while (n > 1)
+        {
+            silnia = silnia * n;
+            n -= 1;
+        }
+        System.out.println("Silnia to: " + silnia);
+
+
+    }
+}
