2b02c63917a4633b8eb175b020a68f8f1ccba033
Maciej <mac.laurek@gmail.com>
2017-08-07 10:47:28 +0200

    BubbleSort done

---
+++ src/main/java/BubbleSort/BubbleSort.java
@@ -0,0 +1,25 @@
+package BubbleSort;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] tab = {12,34,45,65,76,87,56,4,3,2,8,9};
+        int temp;
+        int change = 1;
+
+        while(change > 0){
+            change = 0;
+            for(int i=0; i<tab.length-1; i++){
+                if(tab[i]>tab[i+1]){
+                    temp = tab[i+1];
+                    tab[i+1] = tab[i];
+                    tab[i] = temp;
+                    change++;
+                }
+            }
+        }
+        for(int i=0; i<tab.length; i++){
+            System.out.print(tab[i]+" ");
+        }
+    }
+    }
+
